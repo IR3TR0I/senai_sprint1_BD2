4fed02fea2ce9ffca97b0c53fa3600b5de259b94
Ruan Gustavo <73190735+IR3TR0I@users.noreply.github.com>
2021-03-01 22:32:46 -0300

    Add files via upload

---
+++ Banco de Dados/DQL/SQLLocadoraDQL.sql
@@ -0,0 +1,19 @@
+USE locadora;
+
+SELECT * FROM Marca
+SELECT * FROM Modelo
+SELECT * FROM Cliente
+SELECT * FROM Aluguel
+SELECT * FROM Veiculo
+SELECT * FROM Empresa;
+
+SELECT Empresa.Nome AS Empresa , Empresa.Veiculo AS IdVeiculo , 
+Veiculo.Marca , Veiculo.Modelo , Cliente.Nome , 
+Cliente.Cpf , Aluguel.Valor 
+FROM Empresa
+INNER JOIN Veiculo
+ON Empresa.Veiculo = Veiculo.IdVeiculo
+INNER JOIN Cliente
+ON Empresa.Veiculo = Cliente.IdCliente
+INNER JOIN Aluguel
+ON Empresa.Veiculo = Aluguel.IdAluguel;
